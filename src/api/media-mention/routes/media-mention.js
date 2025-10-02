1de6db5b3b708d35a7fc261f85708be94487b9d1
Gfellerman <ngfeller@gmail.com>
2025-10-02 18:23:48 +0100

    Add all missing controllers, routes, and services for all API endpoints

---
+++ src/api/media-mention/routes/media-mention.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * media-mention router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::media-mention.media-mention');
