66436afeb71c0bda329686243ddf05f32121bc01
Gfellerman <ngfeller@gmail.com>
2025-10-02 18:20:32 +0100

    Fix project controller - restore proper controller logic

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -1,26 +1,9 @@
 'use strict';
 
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/projects',
-      handler: 'project.find',
-      config: {
-        auth: false, // or set to true if you want authentication
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/projects/:id',
-      handler: 'project.findOne',
-      config: {
-        auth: false,
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+/**
+ * project controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::project.project');
