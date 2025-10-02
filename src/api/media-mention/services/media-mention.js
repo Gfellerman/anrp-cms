e1c1b5f7744d232e8d5429f4a0e49cbe0e3aff38
Gfellerman <ngfeller@gmail.com>
2025-10-02 18:03:56 +0000

    Complete Strapi API structure: add missing controllers, routes, and services for all endpoints

---
+++ src/api/media-mention/services/media-mention.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * media-mention service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::media-mention.media-mention');
